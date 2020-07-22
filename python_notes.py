b0015e64e5909debb13748d425cd422c8929664f
Brian Miller <Brismiller@gmail.com>
2020-07-22 14:09:58 -0700

    Update python_notes.py

--- python_notes.py
+++ python_notes.py
@@ -1,8 +1,4 @@
-#!/usr/bin/python2
-#!/usr/bin/python3
-
 # FOLDER HANDLING
-
 # get all filenames in curdir with <str>
 import os
 path = os.curdir
@@ -11,7 +7,6 @@ sfFiles = [i for i in files if i.endswith('.sf')]
 
 
 # FILE HANDLING
-
 # open and load into RAM
 with open("file_name.txt", 'r') as input_handle:
     file_lines_list = input_handle.readlines()
@@ -23,6 +18,9 @@ with open(filename, 'w+') as file:
     file.writelines(i for i in file_lines)
 
 # open a file one line at a time
+with open(filepath) as fp:
+    for cnt, line in enumerate(fp):
+	print(line)
 
 # open a file and keep it open while writing to it
 file=open('test1.txt', 'w+')
@@ -31,8 +29,15 @@ for i in range(1000):
 file.close()
 
 
-# RUN BASH IN PYTHON
+## ARGUMENT HANDLING ##
+import argparse
+parser=argparse.ArgumentParser(description="program that replaces fasta headers")
+parser.add_argument("-i", help="input fasta", type=file)
+parser.add_argument("-o", help="output fasta")
+args = parser.parse_args()
+print("output file: {}".format(args.o)) 
 
+# OTHER RANDOM THINGS
 def bash_cmd(command):
 	"""
 	Calls a bash command and returns the standard output.
@@ -40,3 +45,10 @@ def bash_cmd(command):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True, cwd=os.curdir)
     proc_stdout = process.communicate()[0].strip()
     print("BASH_cmd_stdout: {}".format(proc_stdout))
+
+
+if __name__ == '__main__':
+    execute_order_66()
+
+a="world"
+print("hello {}".format(a))
