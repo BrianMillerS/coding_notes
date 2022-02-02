bcd8f51a35031ccb84e3a060006d4cd3e4383d72
Brian Miller <Brismiller@gmail.com>
2022-02-02 15:30:07 -0700

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,8 +1,10 @@
 # general
 # [[rows], [columns]]
+# 2D = data frame, 1D = series (typically columns)
 df.shape
 df.head
 df.tail
+df.set_index('Date', inplace=True)  # set the row names to be a specific column, implace makes the change permanent
 
 # column selection
 df.columns
@@ -19,3 +21,15 @@ df.loc[0:1 , 'col1':'col3']  # includes row 1 and column 3
 
 # data frame combining
 pd.concat([df1,df2])  # stack dataframes with the same column names
+
+
+# datetime (dt class)
+# https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior (link to datetime custom input symbols)
+df['date'] = pd.to_datetime(df['date'])  # tries to convert the 'date' column into a pandas datetime object
+df['date'] = pd.to_datetime(df['date'], format = '%Y-%m-%d %I-%p') # convert '2020-03-13 20:00:00' to pd datetime
+df[0,'date'].dayname()  # get the day of the week for single value
+df['Date'].dt.day_name()  # get the day of the week for a whole column
+df['Date'].min()
+df['Date'].max()
+filt = (df['Date'] >= pd.to_datetime('2019-01-01'))
+df.loc[filt]
